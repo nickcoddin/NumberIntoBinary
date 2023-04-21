71ec29506189228cb0b485efa1a1ed146af20936
PC <nikoloz.de@gmail.com>
2023-04-21 18:43:34 +0400

    Add project files.

---
+++ NumberTransformation.cs
@@ -0,0 +1,20 @@
+﻿namespace NumberIntoBinary
+{
+    public static class NumberTransformation
+    {
+        public static void InputNumber(string stringNumber)
+        {
+            int inputNumber;
+
+            if(!int.TryParse(stringNumber, out inputNumber))
+            {
+                Console.WriteLine("incorrect input");
+            }
+            else
+            {
+                string binaryNumber = Convert.ToString(inputNumber, 2);
+                Console.WriteLine("number {0} in binary system is: {1}", inputNumber, binaryNumber);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NumberIntoBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("to get binary number, please Enter only Number: ");
+            string? stringNumber = Console.ReadLine();
+            NumberTransformation.InputNumber(stringNumber);
+            Console.ReadKey();
+        }
+    }
+}
